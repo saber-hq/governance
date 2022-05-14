40bb432cec81283ffd9da94a5f186f3046655cc7
Ian Macalinao <ian@saber.so>
2022-05-14 15:15:02 -0500

    pools information

--- build.sh
+++ build.sh
@@ -7,5 +7,6 @@ mkdir out/
 cat config/Sources.toml | yj -t >out/sources.json
 
 cat config/assets/*.toml | yj -t >out/assets.json
+cat config/pools/*.toml | yj -t >out/pools.json
 
 jsonschema schema/sources.schema.json -i ./out/sources.json
